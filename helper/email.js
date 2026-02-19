2d39e0bdf2d57e871dbc6153d5814f12d85f5795
yashguard <yashguard2002@gmail.com>
2026-02-19 21:56:53 +0530

    - Removed resend and use SMTP again

--- helper/email.js
+++ helper/email.js
@@ -1,49 +1,49 @@
 import fs from "fs";
 import nodemailer from "nodemailer";
-import {config} from "../config/config.js";
+import { config } from "../config/config.js";
 import path from "path";
 
 const mailTransport = nodemailer.createTransport({
-    host: config.email.smtp.host,
-    port: config.email.smtp.port,
-    secure: false,
-    auth: {
-        user: config.email.smtp.auth.user,
-        pass: config.email.smtp.auth.pass,
-    },
+  host: config.email.smtp.host,
+  port: config.email.smtp.port,
+  secure: false,
+  auth: {
+    user: config.email.smtp.auth.user,
+    pass: config.email.smtp.auth.pass,
+  },
 });
-
 
 /**
  * Send mail function
  */
-export const sendMail = async ({to,subject,dynamicData,filename}) => {
-    let html = fs.readFileSync(path.join(process.cwd(),"pages",filename),
-        "utf-8"
-    );
-    Object.keys(dynamicData).forEach((key) => {
-        const regex = new RegExp(`\\{\\{${ key }\\}\\}`,"g");
-        html = html.replace(regex,dynamicData[ key ]);
-    });
+export const sendMail = async ({ to, subject, dynamicData, filename }) => {
+  let html = fs.readFileSync(
+    path.join(process.cwd(), "pages", filename),
+    "utf-8",
+  );
+  Object.keys(dynamicData).forEach((key) => {
+    const regex = new RegExp(`\\{\\{${key}\\}\\}`, "g");
+    html = html.replace(regex, dynamicData[key]);
+  });
 
-    const result = mailTransport.sendMail(
-        {
-            from: config.email.smtp.from,
-            to,
-            subject,
-            html,
-        },
-        async (err,info) => {
-            if (err) {
-                console.log("err: ",err.message);
-                return err.message;
-            } else {
-                console.log(`mail successfully sent on ${ info.accepted[ 0 ] }`);
-                return info;
-            }
-        }
-    );
-    return result;
+  const result = mailTransport.sendMail(
+    {
+      from: config.email.smtp.from,
+      to,
+      subject,
+      html,
+    },
+    async (err, info) => {
+      if (err) {
+        console.log("err: ", err.message);
+        return err.message;
+      } else {
+        console.log(`mail successfully sent on ${info.accepted[0]}`);
+        return info;
+      }
+    },
+  );
+  return result;
 };
 
 // import fs from "fs";
